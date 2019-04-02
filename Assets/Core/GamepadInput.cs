9df1ead63ac92d3c4dc3dc0e39d41b9d726a0f1e
Patryk Zielinski <patryk.zielinski93@gmail.com>
2019-04-02 21:35:18 +0200

    initial commit

---
+++ Assets/Core/GamepadInput.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GamepadButton
+{
+    ButtonA,
+    ButtonB,
+    ButtonX,
+    ButtonY,
+    LBumper,
+    RBumper
+}
+
+public enum GamepadJoystick
+{
+    LeftJoystickHorizontal,
+    LeftJoystickVertical,
+    RightJoystickHorizontal,
+    RightJoystickVertical
+}
+
+public class GamepadInput
+{
+    public int gamepadNumber;
+
+    private Dictionary<GamepadButton, string> buttons = new Dictionary<GamepadButton, string>();
+    private Dictionary<GamepadJoystick, string> joysticks = new Dictionary<GamepadJoystick, string>();
+
+    public bool IsUp(GamepadButton gamepadButton)
+    {
+        return Input.GetButtonUp(buttons[gamepadButton]);
+    }
+
+    public bool IsDown(GamepadButton gamepadButton)
+    {
+        return Input.GetButtonDown(buttons[gamepadButton]);
+    }
+
+    public bool IsPressed(GamepadButton gamepadButton)
+    {
+        return Input.GetButton(buttons[gamepadButton]);
+    }
+
+    public float GetJoystickAxis(GamepadJoystick gamepadJoystick)
+    {
+        return Input.GetAxis(joysticks[gamepadJoystick]);
+    }
+
+    public void InitController(int number)
+    {
+        gamepadNumber = number;
+        buttons.Clear();
+        joysticks.Clear();
+        InitButtons(number);
+        InitJoysticks(number);
+    }
+
+    private void InitButtons(int number)
+    {
+        foreach (GamepadButton jb in Enum.GetValues(typeof(GamepadButton)))
+        {
+            buttons.Add(jb, "J" + number + jb);
+        }
+    }
+
+    private void InitJoysticks(int number)
+    {
+        foreach (GamepadJoystick gj in Enum.GetValues(typeof(GamepadJoystick)))
+        {
+            joysticks.Add(gj, "J" + number + gj);
+//            Debug.Log("J" + number + gj);
+        }
+    }
+}
---
+++ Assets/GameController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+
+public class GameController : MonoBehaviour
+{
+    private GamepadInput ci1 = new GamepadInput();
+
+    private GamepadInput ci2 = new GamepadInput();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ci1.InitController(1);
+        ci2.InitController(2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var x1 = ci1.GetJoystickAxis(GamepadJoystick.LeftJoystickVertical);
+        var x3 = ci1.GetJoystickAxis(GamepadJoystick.LeftJoystickHorizontal);
+        var x2 = ci1.GetJoystickAxis(GamepadJoystick.RightJoystickVertical);
+        var x4 = ci1.GetJoystickAxis(GamepadJoystick.RightJoystickHorizontal);
+
+
+        if (x1 != 0)
+        {
+            Debug.Log("Left vertical: " + x1);
+        }
+        
+        if (x2 != 0)
+        {
+            Debug.Log("Right vertical: " + x2);
+        }
+        
+        if (x3 != 0)
+        {
+            Debug.Log("Left horizontal: " + x3);
+        }
+        
+        if (x4 != 0)
+        {
+            Debug.Log("Right horizontal: " + x4);
+        }
+    }
+}
