c068a4e113c036e9759d9dfe27b2633d01475e2c
Patryk Zielinski <patryk.zielinski93@gmail.com>
2019-04-07 14:24:32 +0200

    Add displaying player characters and scores on ScoreBoard

--- Assets/Scenes/ScoreBoard/Scripts/ScoreBoardController.cs
+++ Assets/Scenes/ScoreBoard/Scripts/ScoreBoardController.cs
@@ -1,18 +1,55 @@
-﻿using UnityEngine;
+﻿using Core;
+using UnityEngine;
 
 namespace Scenes.ScoreBoard.Scripts
 {
-    public class ScoreBoardController : MonoBehaviour
+    public class ScoreBoardController : BaseGameController
     {
-        // Start is called before the first frame update
-        void Start()
+        public int wonRoundsToWin = 3;
+        public GameObject scorePrefab;
+
+        private ScoreComponent _scoreComponent;
+
+        private readonly Vector2[] _playerPositions =
         {
-            Physics2D.gravity = Vector2.zero;
+            new Vector2(-7.5f, 0),
+            new Vector2(-2.5f, 0),
+            new Vector2(2.5f, 0),
+            new Vector2(7.5f, 0)
+        };
+
+        private readonly Vector2[] _scorePositions =
+        {
+            new Vector2(-7.5f, 1f),
+            new Vector2(-2.5f, 1f),
+            new Vector2(2.5f, 1f),
+            new Vector2(7.5f, 1f)
+        };
+
+        private void Awake()
+        {
+            // Debug purposes
+            if (GameState.Instance.GetAllPlayers().Length == 0)
+            {
+                GameState.Instance.AddPlayers(4);
+            }
         }
 
-        // Update is called once per frame
-        void Update()
+
+        // Start is called before the first frame update
+        new void Start()
         {
+            base.Start();
+            Physics2D.gravity = Vector2.zero;
+
+            SpawnPlayersCharacters(_playerPositions);
+
+            _scoreComponent = scorePrefab.GetComponent<ScoreComponent>();
+
+            for (int i = 0; i < players.Length; i++)
+            {
+                _scoreComponent.ShowScore(players[i].globalScore, wonRoundsToWin, _scorePositions[i], transform);
+            }
         }
     }
 }
---
+++ Assets/ScoreComponent.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class ScoreComponent : MonoBehaviour
+{
+    public GameObject goldScorePrefab;
+    public GameObject goldScoreTopPrefab;
+    public GameObject silverScorePrefab;
+    public GameObject silverScoreTopPrefab;
+
+    private GameObject[] _scoreElements;
+
+    public void ShowScore(int currentScore, int maxScore, Vector2 position, Transform trans)
+    {
+        _scoreElements = new GameObject[maxScore];
+        Vector2 pos = position;
+        
+        for (int i = 1; i <= maxScore; i++)
+        {
+            pos = i == 1 ? pos : new Vector2(pos.x, pos.y + (i == maxScore ? 0.23f : 0.15f));
+            _scoreElements[i - 1] =
+                Instantiate(GetScoreElement(i, currentScore, maxScore), pos, Quaternion.identity, trans);
+        }
+    }
+
+    private GameObject GetScoreElement(int currentElementIndex, int currentScore, int maxScore)
+    {
+        if (currentElementIndex == maxScore)
+        {
+            return currentScore == maxScore ? goldScoreTopPrefab : silverScoreTopPrefab;
+        }
+
+        return currentScore >= currentElementIndex ? goldScorePrefab : silverScorePrefab;
+    }
+}
