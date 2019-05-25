7028d588a3e22e1f266d4286941efbff99972484
michalek <michal.mendygrall@gmail.com>
2019-05-25 15:39:38 +0200

    first_time

--- Assets/Core/GameRandomizer.cs
+++ Assets/Core/GameRandomizer.cs
@@ -15,8 +15,9 @@ namespace Core
         {
             _availableGames = new List<int>
             {
-                (int) AvailableScene.FlappyRun,
-                (int) AvailableScene.RaceToTheCashRegister
+                //(int) AvailableScene.FlappyRun,
+                //(int) AvailableScene.RaceToTheCashRegister
+                (int) AvailableScene.AvoidObstacles
             };
         }
 
---
+++ Assets/Scenes/AvoidObstacles/Scripts/destroyerTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroyerTrigger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //Debug.Log("zderzylem sie 0");
+        if (collision.gameObject.tag == "platform_stuff" || collision.gameObject.tag == "enemies")
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+
+
+
+}
