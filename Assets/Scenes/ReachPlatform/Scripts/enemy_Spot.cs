2b34b1b961bbdbb6c4eaa9ee6ced4c1ab39000c9
michalek <michal.mendygrall@gmail.com>
2019-05-26 10:44:48 +0200

    Reach Platform game added

---
+++ Assets/Scenes/ReachPlatform/Scripts/enemy_Spot.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy_Spot : MonoBehaviour
+{
+    public GameObject[] enemies;
+    int enemyNo;
+    public float maxPos = 2.2f;
+    public float delayTimer = .9f;
+    float timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = delayTimer;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+        {
+            Vector3 enemyPos = new Vector3(Random.Range(-6f, 4f), transform.position.y,  transform.position.z);
+            enemyNo = Random.Range(0, 1);
+            Instantiate(enemies[enemyNo], enemyPos, transform.rotation);
+            timer = delayTimer;
+        }
+
+    }
+}
---
+++ Assets/Scenes/ReachPlatform/Scripts/platformsSpot.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class platformsSpot : MonoBehaviour
+{
+    public bool platforms_start = false;
+    public GameObject[] platforms;
+    int paltformNo;
+    //public float maxPos = 4.2f;
+    public float delayTimer;
+    float timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = delayTimer;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (platforms_start == true)
+        {
+            timer -= Time.deltaTime;
+            if (timer <= 0)
+            {
+                Vector3 platformPos = new Vector3(Random.Range(-6f, 4f), transform.position.y, transform.position.z);
+                paltformNo = Random.Range(0, 4);
+                Instantiate(platforms[paltformNo], platformPos, transform.rotation);
+                timer = delayTimer;
+            }
+        }
+
+    }
+}
